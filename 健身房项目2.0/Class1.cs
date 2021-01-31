52152742a28be4cb1adf3d5de2358b46773b5db0
linyixin <linyixin2002>
2021-01-31 17:08:36 +0800

    学生个人制作健身房项目 windform

---
+++ 健身房项目2.0/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 健身房项目2._0
+{
+    public class Class1
+    {
+    }
+}
---
+++ 健身房项目2.0/DAL/DBHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace DAL
+{
+    public class DBHelper
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=健身房管理系统;Integrated Security=True");
+        public DataTable cx(string sql)
+        {
+            SqlDataAdapter sdr = new SqlDataAdapter(sql,con );
+            DataTable dt = new DataTable();
+            sdr.Fill(dt);
+            return dt;
+        }
+
+        public int zsg(string sql)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql,con);
+            int a = cmd.ExecuteNonQuery();
+            con.Close();
+            return a;
+        }
+    }
+
+}
